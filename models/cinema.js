13608093e18cb4cf39165160b20908015771667d
idmacleod <idmacleod@gmail.com>
2020-05-06 14:17:10 +0100

    Adding filterByGenre

--- models/cinema.js
+++ models/cinema.js
@@ -10,4 +10,8 @@ Cinema.prototype.findByTitle = function (title) {
   return this.films.find(film => film.title === title); 
 }
 
+Cinema.prototype.filterByGenre = function (genre) {
+  return this.films.filter(film => film.genre === genre);
+}
+
 module.exports = Cinema;
